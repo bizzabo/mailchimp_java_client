1aa3e6dc7fc6e771ec9681f62429e28d6ad49dce
Zachi Shema <zachi@bizzabo.com>
2016-03-08 22:08:33 +0200

    clean main

--- src/main/java/com/bizzabo/provider/mailchimp/Mailchimp.java
+++ src/main/java/com/bizzabo/provider/mailchimp/Mailchimp.java
@@ -1,9 +1,6 @@
 package com.bizzabo.provider.mailchimp;
 
 import com.bizzabo.provider.mailchimp.api.ListsV3API;
-import com.bizzabo.provider.mailchimp.model.MailchimpCategories;
-import com.bizzabo.provider.mailchimp.model.MailchimpInterest;
-import com.bizzabo.provider.mailchimp.model.MailchimpInterests;
 
 public class Mailchimp
 {
@@ -19,9 +16,4 @@ public class Mailchimp
 		return lists;
 	}
 	
-	public static void main(String[] args)
-	{
-		Mailchimp mailchimp = new Mailchimp("https://us12.api.mailchimp.com", "5ec33fddcd32c8c251b69ff1deb3fa2e");
-		MailchimpInterests interests = mailchimp.lists().getInterests("dbf29cd2c2","76ab6cae80");
-	}
 }
